b7d224447bbca5967b346c49b5a5409e6f840645
Robin Erlacher <rerlache@gmail.com>
2022-01-12 10:42:22 +0100

    added function to get all the harddrive informations

--- OldFileCleanupApp/MainWindow.xaml.cs
+++ OldFileCleanupApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,40 @@ namespace OldFileCleanup
         public MainWindow()
         {
             InitializeComponent();
+            GetDriveInformations();
+        }
+        public void GetDriveInformations()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            drivesComboBox.ItemsSource = allDrives;
+            /* -- the following loop can be used in a console application, need to be rebuild for WPF --
+            foreach (DriveInfo d in allDrives)
+            {
+                Console.WriteLine("Drive {0}", d.Name);
+                Console.WriteLine("  Drive type: {0}", d.DriveType);
+                if (d.IsReady == true)
+                {
+                    Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
+                    Console.WriteLine("  File system: {0}", d.DriveFormat);
+                    Console.WriteLine(
+                        "  Available space to current user:{0, 15} bytes",
+                        d.AvailableFreeSpace);
+
+                    Console.WriteLine(
+                        "  Total available space:          {0, 15} bytes",
+                        d.TotalFreeSpace);
+
+                    Console.WriteLine(
+                        "  Total size of drive:            {0, 15} bytes ",
+                        d.TotalSize);
+                }
+            */
+        }
+
+        private void LoadDrivesButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetDriveInformations();
         }
     }
 }
+
